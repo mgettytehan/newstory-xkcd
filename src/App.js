05138f15ca873cd4e95d9b977fd937eceec0b5bc
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-10-27 15:07:50 -0400

    Adds some basic styling to overall app and nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom';
+
+import './App.css';
+
 import Latest from './components/pages/Latest.js';
 import Search from './components/pages/Search.js'
 import { notFound } from './components/pages/NotFound.js'
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <nav>
-        <NavLink exact to="/" className="latest" activeStyle={{color: "red"}}>Latest</NavLink>
-        <NavLink exact to="/search" className="search" activeStyle={{color: "red"}}>Search</NavLink>
+        <NavLink exact to="/" className="latest" activeClassName="selected">Latest</NavLink>
+        <NavLink exact to="/search" className="search" activeClassName="selected">Search</NavLink>
       </nav>
       <main>
         <Switch>
