e24babc7be281436b6e0a5121161a3ced026a4d2
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-10-27 14:23:00 -0400

    Adds react-router-dom for navigation and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import Latest from './components/pages/Latest.js';
 import Search from './components/pages/Search.js'
 
 function App() {
   return (
     <div>
-      <Latest />
-      <Search />
+      <nav>
+        <NavLink exact to="/" className="latest" activeStyle={{color: "red"}}>Latest</NavLink>
+        <NavLink exact to="/search" className="search" activeStyle={{color: "red"}}>Search</NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/" component={Latest} />
+        </Switch>
+      </main>
     </div>
   );
 }
